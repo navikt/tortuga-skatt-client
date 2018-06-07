6a64cd2b91296646bde45467c1b25f6bfeb515db
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-06-07 15:10:44 +0200

    Actually use our own factory ...

--- src/main/java/no/nav/opptjening/skatt/client/api/AbstractClient.java
+++ src/main/java/no/nav/opptjening/skatt/client/api/AbstractClient.java
@@ -17,7 +17,6 @@ import retrofit2.Call;
 import retrofit2.Converter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
